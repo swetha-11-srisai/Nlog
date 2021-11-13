e3824519c1fbf7236eeb6b2e3ebd3a8e4650c4ba
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-13 14:52:53 -0800

    [Swetha] add : Nlog program added

---
+++ NLogProject/NLogProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NLogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers add = new AddNumbers();
+            add.Sum(34,45);
+        }
+    }
+}
